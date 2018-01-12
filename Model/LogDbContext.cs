f8cb904ba023926ed7875e88f03b4f7113386a91
GCnaoto <gcnaoto2@gmail.com>
2018-01-12 23:54:21 +0900

    イニシャライザーを追加

--- Model/LogDbContext.cs
+++ Model/LogDbContext.cs
@@ -1,14 +1,20 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace AspNetCoreNlog.Model
 {
     public class LogDbContext : DbContext
     {
+        private readonly ILogger _logger;
+        private readonly ILoggerFactory _loggerfact;
 
-        public LogDbContext(DbContextOptions<LogDbContext> options)
+        public LogDbContext(DbContextOptions<LogDbContext> options, ILoggerFactory logger)
         {
-            Database.EnsureCreated();
+            //_logger = logger.CreateLogger("TodoApi.Controllers.TodoController");
+            //Database.EnsureCreated();
         }
 
         public DbSet<Logs> Logs { get; set; }
@@ -18,6 +24,26 @@ namespace AspNetCoreNlog.Model
             
             base.OnModelCreating(modelBuilder);
 
+        }
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            var serviceProvider = this.GetInfrastructure();
+            //var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            //loggerFactory.AddProvider(new LoggerProvider(logAction));
+            _loggerfact.AddDebug().CreateLogger<LogDbContext>().LogError("更新してます");
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.State == EntityState.Added))
+            {
+                _loggerfact.AddDebug().CreateLogger<LogDbContext>().LogError("更新してます");
+
+                //modify entry.Entity here
+            }
+
+            ChangeTracker.AutoDetectChangesEnabled = false;
+            var result = base.SaveChanges();
+            ChangeTracker.AutoDetectChangesEnabled = true;
+
+            return result;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
